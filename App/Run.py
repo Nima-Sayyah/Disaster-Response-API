f9a128f570436b9db638709e875c819b75f028ca
Nima Sayyah <81308966+Nima-Sayyah@users.noreply.github.com>
2022-01-08 23:38:19 +0000

    Update 27 - Run.py

--- App/Run.py
+++ App/Run.py
@@ -33,3 +33,110 @@ def tokenize(text):
 # Loading data
 engine = create_engine('sqlite:///../Data/disaster_messages_database.db')
 df = pd.read_sql_table('disaster_messages_tbl', engine)
+
+# Loading model
+model = joblib.load("../Models/classifier.pkl")
+
+# Indexing webpage that displays visuals and receives user input text for model
+# Flask decorator to assign URLs - dictates @app when user visits (app.com) at the given .route() execute the home() function
+@app.route('/')
+@app.route('/index')
+
+def index():
+    
+   # Extreacting specific data for visualization 
+    genre_counts = df.groupby('genre').count()['message']
+    genre_names = list(genre_counts.index)
+    
+    # Categories column names
+    catg_nam = df.iloc[:, 4:].columns
+    # Selecting those with none-zero values
+    bol = df.iloc[:, 4:] != 0
+    # Extracting the number of none-zero values
+    cat_bol = bol.sum().values
+    # Extracting sum of all values per column
+    sum_cat = df.iloc[:, 4:].sum()
+    # Extracting top-10 values
+    top_cat = sum_cat.sort_values(ascending=False)[1:11]
+    # Extracting those columns name
+    top_cat_names = list(top_cat.index)
+
+    graphs = [
+        {
+            'data': [
+                Bar(
+                    x=genre_names,
+                    y=genre_counts
+                )
+            ],
+
+            'layout': {
+                'title': 'Distribution of Message Genres',
+                'yaxis': {
+                    'title': "Count"
+                },
+                'xaxis': {
+                    'title': "Genre"
+                }
+            }
+        },
+        {
+            'data': [
+                Bar(
+                    x=catg_nam,
+                    y=cat_bol
+                )
+            ],
+
+            'layout': {
+                'title': 'Message Categories distribution',
+                'yaxis': {
+                    'title': "Count"
+                },
+                'xaxis': {
+                    'title': "Categories"
+                }
+            }
+        },
+        {
+            'data': [
+                Bar(
+                    x=top_cat_names,
+                    y=top_cat
+                )
+            ],
+
+            'layout': {
+                'title': 'Top 10 Categories',
+                'yaxis': {
+                    'title': "Count"
+                },
+                'xaxis': {
+                    'title': "Categories"
+                }
+            }
+        }
+    ]
+
+    # Encoding plotly graphs in JSON
+    ids = ["graph-{}".format(i) for i, _ in enumerate(graphs)]
+    graphJSON = json.dumps(graphs, cls=plotly.utils.PlotlyJSONEncoder)
+    
+    # Rendering web-page with plotly graphs
+    return render_template('master.html', ids=ids, graphJSON=graphJSON)
+
+# Indexing webpage that handles user query and displays model results
+@app.route('/go')
+
+def go():
+    # This saves user input in query (A MultiDict is a dictionary subclass customized to save all values for a key)
+    # "get()"" method returns the default value (second argument) if the requested data doesn’t exist.
+    query = request.args.get('query', '') 
+
+    # Using the model to predict the query - the result is an array as many as our label columns 
+    classification_labels = model.predict([query])[0]
+    # Assigning each label to its corresonding column name to form an iterable: Dict
+    classification_results = dict(zip(df.columns[4:], classification_labels))
+
+    # Rendering the go.html web-page 
+    return render_template('go.html', query=query, classification_result=classification_results)
